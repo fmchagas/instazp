3d975fb75927ca7a2dc99c122512d893079564bf
Fernando Macedo <fermaccha@gmail.com>
2020-08-25 20:41:45 -0400

    Configura start da aplicação

---
+++ src/main/java/br/com/fmchagas/intazp/IntazpApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmchagas.intazp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntazpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntazpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fmchagas/intazp/IntazpApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmchagas.intazp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntazpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
